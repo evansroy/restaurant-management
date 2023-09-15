833178155c5bdbe727515cfff707a0ac174866bc
Evans Roy <evansroysir@gmail.com>
2023-09-15 19:35:15 +0300

    Initial commit With SingUp and Home  component

---
+++ src/components/SignUp.vue
@@ -0,0 +1,83 @@
+
+<template>
+    <div>
+      <img class="logo" alt="logo" src="../assets/logo.png">
+      <h1>Sign Up</h1>
+      <div class="register">
+        <input type="text" v-model="name" placeholder="Enter Name"/>
+        <input type="email" v-model="email" placeholder="Enter Email"/>
+        <input type="password" v-model="password" placeholder="Enter Password"/>
+        <button @click="signUp">Sign Up</button>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  
+  export default {
+    name: 'SignUp',
+    data() {
+      return {
+        name: '',
+        email: '',
+        password: ''
+      }
+    },
+    methods: {
+      async signUp() {
+        try {
+          let result = await axios.post("http://localhost:3000/user", {
+            name: this.name,
+            email: this.email,
+            password: this.password
+          });
+  
+          console.log(result);
+  
+          if (result.status === 201) {
+            localStorage.setItem("user-info", JSON.stringify(result.data));
+            this.$router.push({ name: 'Home' });
+          }
+        } catch (error) {
+          console.error(error); // Handle the error appropriately (e.g., show an error message to the user)
+        }
+      }
+    },
+    computed: {
+      isLoggedIn() {
+        return !!localStorage.getItem('user-info');
+      }
+    },
+    mounted() {
+      if (this.isLoggedIn) {
+        this.$router.push({ name: 'Home' });
+      }
+    }
+  }
+  </script>
+
+<style>
+.logo {
+    width:100px;
+}
+.register input {
+    width: 300px;
+    height: 40px;
+    padding-left: 20px;
+    display: block;
+    margin-bottom:30px;
+    margin-right: auto;
+    margin-left: auto;
+    border: 1px solid #fab81e;
+}
+
+.register button{
+    width: 320px;
+    height: 40px;
+    border: 1px solid #fab81e;
+    background: #fab81e;
+    color: #fff;
+    cursor: pointer;
+}
+</style>
