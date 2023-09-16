b81755ae57cb91c39f62c8f8dae300a3789f57c0
Evans Roy <evansroysir@gmail.com>
2023-09-16 23:35:46 +0300

    Add Add and Update View

---
+++ src/components/Add.vue
@@ -0,0 +1,21 @@
+<template>
+    <Header/>
+    <h1>Hello User, Welcome on Add Resaurant Page</h1>
+</template>
+<script>
+    import Header from './Header.vue'
+    export default {
+        name:'AddView',
+        components:{
+            Header
+        },
+        mounted()
+            {
+                let user = localStorage.getItem('user-info');
+                if(!user)
+                {
+                    this.$router.push({name:'SignUp'})
+                }
+            }
+    }
+</script>
--- src/routers.js
+++ src/routers.js
@@ -2,6 +2,8 @@ import Home from "./components/Home.vue";
 import SignUp from "./components/SignUp.vue";
 import { createRouter, createWebHistory } from "vue-router";
 import LogIn from './components/LogIn.vue';
+import Add from './components/Add.vue';
+import Update from './components/Update.vue';
 const routes = [
   {
     name: "Home",
@@ -17,6 +19,16 @@ const routes = [
     name: "LogIn",
     component:LogIn,
     path: "/log-in",
+  },
+  {
+    name: "Add",
+    component:Add,
+    path: "/add",
+  },
+  {
+    name: "Update",
+    component:Update,
+    path: "/update",
   }
 
 ];
