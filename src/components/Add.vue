0f04b7800217be2a549f191cdfe1652c74630503
Evans Roy <evansroysir@gmail.com>
2023-09-17 06:06:29 +0300

    Add Restaurant Functionality

--- src/components/Add.vue
+++ src/components/Add.vue
@@ -10,6 +10,7 @@
 </template>
 <script>
 import Header from './Header.vue'
+import axios from 'axios';
 export default {
     name: 'AddView',
     components: {
@@ -26,8 +27,19 @@ export default {
         }
     },
     methods: {
-        addRestaurant() {
+        async addRestaurant() {
+
             console.log(this.restaurant);
+            const result = await axios.post("http://localhost:3000/restaurant",{
+                name:this.restaurant.name,
+                address:this.restaurant.address,
+                contacts:this.restaurant.contact,
+            });
+            if(result.status==201)
+            {
+                this.$router.push({name:'Home'})
+            }
+            console.log("result", result);
         }
     },
     mounted() {
