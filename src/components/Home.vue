f4fbcc948a6b2145efef351c43cb5b327e41ecb9
Evans Roy <evansroysir@gmail.com>
2023-09-17 05:19:42 +0300

    Add restaurant api and populate them in a table

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,20 +1,36 @@
 <template>
     <Header/>
     <h1>Hello {{name}}, Welcome on Home Page</h1>
+    <table border=".5">
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Contacts</th>
+            <th>Address</th>
+        </tr>
+        <tr v-for="item in restaurant" :key="item.id">
+            <td>{{ item.id }}</td>
+            <td>{{ item.name }}</td>
+            <td>{{ item.contacts }}</td>
+            <td>{{ item.address }}</td>
+        </tr>
+    </table>
 </template>
 <script>
     import Header from './Header.vue'
+    import axios from 'axios';
     export default {
         name:'HomePage',
         data(){
             return {
-                name:''
+                name:'',
+                restaurant:[],
             }
         },
         components:{
             Header
         },
-        mounted()
+       async mounted()
             {
                 let user = localStorage.getItem('user-info');
                 this.name = JSON.parse(user).name;
@@ -22,6 +38,16 @@
                 {
                     this.$router.push({name:'SignUp'})
                 }
+
+                let result = await axios.get("http://localhost:3000/restaurant");
+                console.log(result);
+                this.restaurant = result.data;
             }
     }
 </script>
+<style>
+    td{
+        width: 160px;
+        height: 40px;
+    }
+</style>
