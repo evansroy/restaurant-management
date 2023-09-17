d3983d96729fc5bb6bd5693dfd85d1dc8aa3406d
Evans Roy <evansroysir@gmail.com>
2023-09-17 04:51:27 +0300

    Display LoggedIn Username on homepage

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,17 +1,23 @@
 <template>
     <Header/>
-    <h1>Hello User, Welcome on Home Page</h1>
+    <h1>Hello {{name}}, Welcome on Home Page</h1>
 </template>
 <script>
     import Header from './Header.vue'
     export default {
         name:'HomePage',
+        data(){
+            return {
+                name:''
+            }
+        },
         components:{
             Header
         },
         mounted()
             {
                 let user = localStorage.getItem('user-info');
+                this.name = JSON.parse(user).name;
                 if(!user)
                 {
                     this.$router.push({name:'SignUp'})
