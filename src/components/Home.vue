868e1324f3d20ac48faefea4dcd76693be41572f
Evans Roy <evansroysir@gmail.com>
2023-09-17 15:50:06 +0300

    Add Delete Functionality

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,6 @@
 <template>
-    <Header/>
-    <h1>Hello {{name}}, Welcome on Home Page</h1>
+    <Header />
+    <h1>Hello {{ name }}, Welcome on Home Page</h1>
     <table border=".5">
         <tr>
             <th>ID</th>
@@ -14,27 +14,38 @@
             <td>{{ item.name }}</td>
             <td>{{ item.contacts }}</td>
             <td>{{ item.address }}</td>
-            <td><router-link :to="'/update/' + item.id">Update</router-link></td>
+            <td>
+                <router-link :to="'/update/' + item.id">Update</router-link>
+                <button v-on:click="deleteRestaurant(item.id)">Delete</button>
+            </td>
         </tr>
     </table>
 </template>
 <script>
-    import Header from './Header.vue'
-    import axios from 'axios';
-    export default {
-        name:'HomePage',
-        data(){
-            return {
-                name:'',
-                restaurant:[],
+import Header from './Header.vue'
+import axios from 'axios';
+export default {
+    name: 'HomePage',
+    data() {
+        return {
+            name: '',
+            restaurant: [],
+        }
+    },
+    components: {
+        Header
+    },
+    methods: {
+        async deleteRestaurant(id) {
+            let result = await axios.delete("http://localhost:3000/restaurant/" + id);
+            console.warn(result);
+            if (result.status == 200) {
+                this.loadData()
             }
         },
-        components:{
-            Header
-        },
-       async mounted()
-            {
-                let user = localStorage.getItem('user-info');
+        async loadData()
+        {
+            let user = localStorage.getItem('user-info');
                 this.name = JSON.parse(user).name;
                 if(!user)
                 {
@@ -44,12 +55,19 @@
                 let result = await axios.get("http://localhost:3000/restaurant");
                 console.log(result);
                 this.restaurant = result.data;
-            }
+
+        }
+
+    },
+    async mounted()
+    {
+        this.loadData()
     }
+}
 </script>
 <style>
-    td{
-        width: 160px;
-        height: 40px;
-    }
+td {
+    width: 160px;
+    height: 40px;
+}
 </style>
